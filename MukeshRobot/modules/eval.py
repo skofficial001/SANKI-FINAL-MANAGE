5526f6fa157a13bbac0848cb0f4c42c5e4bb0382
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-01-12 02:23:09 +0530

    Update and rename eval (2).py to eval.py

--- MukeshRobot/modules/eval.py
+++ MukeshRobot/modules/eval.py
@@ -33,8 +33,8 @@ from contextlib import redirect_stdout
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from DAXXROBOT import LOGGER, dispatcher,OWNER_ID
-from DAXXROBOT.modules.helper_funcs.chat_status import dev_plus
+from MukeshRobot import LOGGER, dispatcher,OWNER_ID
+from MukeshRobot.modules.helper_funcs.chat_status import dev_plus
 
 namespaces = {}
 
