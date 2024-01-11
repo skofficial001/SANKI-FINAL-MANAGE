1ca0179d3a3f11f89c278b11b2436fc2fcd27c15
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-01-12 00:51:28 +0530

    Update afk_sql.py

--- MukeshRobot/modules/sql/afk_sql.py
+++ MukeshRobot/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Boolean, Column, UnicodeText
 
-from DAXXROBOT.modules.sql import BASE, SESSION
+from MukeshRobot.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
